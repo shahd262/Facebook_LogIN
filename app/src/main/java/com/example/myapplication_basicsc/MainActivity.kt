7771d9517797f14976ffec548c9b1733b1ce046b
shahd <shahddmohamedd2007@gmail.com>
2025-02-12 02:03:51 +0200

    facebook login screen

---
+++ app/src/main/java/com/example/myapplication_basicsc/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication_basicsc
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        }
+}
+
